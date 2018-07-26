f2ef2f30dc11f6059b1f4657b6375fd2dd30e8b8
Sebastien Duthil <sduthil@wazo.community>
2018-07-26 11:25:18 -0400

    add recipe for nestbox

---
+++ README.md
@@ -0,0 +1,20 @@
+# wazo-ansible
+
+## Prerequisites for Nestbox
+
+* You need root access on `mirror.wazo.community`.
+* You need a Debian Stretch vanilla, reachable under the name `nestbox-dev-ansible` (via `/etc/hosts` for example).
+* You can SSH easily into this machine.
+
+## How to install Nestbox
+
+* Install Ansible: `pip install ansible`
+* Run `ansible-playbook -i dev mirror_keys.yml --extra-vars nestbox_mirror_user=test`
+* Run `ansible-playbook -i dev nestbox.yml`
+* Nestbox will be installed on `nestbox-dev-ansible`
+
+## File hierarchy
+
+The hierarchy follows the best practices described here:
+
+https://docs.ansible.com/ansible/latest/user_guide/playbooks_best_practices.html#directory-layout
---
+++ roles/nestbox/files/wazo-auth-init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Add certificate in auth section:
+cat > ~/.config/wazo-auth-cli/50-certificate.yml <<EOF
+auth:
+    verify_certificate: /etc/nestbox/https/public-certificate.pem
+EOF
+
+# Set wazo-auth-cli config
+export WAZO_AUTH_CLI_CONFIG=~/.config/wazo-auth-cli
+cat > ~/.bashrc <<EOF
+export WAZO_AUTH_CLI_CONFIG=~/.config/wazo-auth-cli
+EOF
+
+# Create your super user policy
+NESTBOX_POLICY=$(wazo-auth-cli policy create --description "Default super user nestbox policy" --acl "auth.#" "deployd.#" "confd.#" -- nestbox_default_super_user)
+
+# Create your super user
+NESTBOX_TENANT=$(wazo-auth-cli tenant create admin-tenant)
+NESTBOX_USER=$(wazo-auth-cli user create --firstname John --lastname Doe --password secret --email "john@example.com" --tenant $NESTBOX_TENANT root)
+wazo-auth-cli user add --policy $NESTBOX_POLICY $NESTBOX_USER
+
+# Add the cli policy to the cli user
+NESTBOX_CLI_POLICY=$(wazo-auth-cli policy create --description "wazo-auth-cli policy" --acl "auth.#" -- wazo_auth_cli_policy)
+wazo-auth-cli user add --policy $NESTBOX_CLI_POLICY wazo-auth-cli
+
+# Clean Wazo default policy
+wazo-auth-cli policy delete wazo_default_user_policy
+wazo-auth-cli policy delete wazo_default_admin_policy
+wazo-auth-cli policy delete wazo_default_master_user_policy
+
+# Create defaults policies for Reseller/Customer/Location
+NESTBOX_RESELLER_POLICY=$(wazo-auth-cli policy create --description "Default reseller nestbox policy" --acl "auth.users.#" "auth.tenants.#" "auth.groups.#" "auth.policies.#" "deployd.#" "confd.#" -- nestbox_default_reseller)
+NESTBOX_CUSTOMER_POLICY=$(wazo-auth-cli policy create --description "Default customer nestbox policy" --acl "auth.users.me.#" "deployd.instances.*.credentials.#.read" "confd.#" -- nestbox_default_customer)
+NESTBOX_LOCATION_POLICY=$(wazo-auth-cli policy create --description "Default location nestbox policy" --acl "auth.users.me.#" "deployd.instances.*.credentials.#.read" "confd.#" -- nestbox_default_location)
+
+mkdir -p /etc/nestbox-ui/conf.d
+cat >/etc/nestbox-ui/conf.d/50-default-policies.yml <<EOL
+policies:
+    reseller: ${NESTBOX_RESELLER_POLICY}
+    customer: ${NESTBOX_CUSTOMER_POLICY}
+    location: ${NESTBOX_LOCATION_POLICY}
+EOL
